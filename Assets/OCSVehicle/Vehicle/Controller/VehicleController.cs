5ce28c462bacdc00a7ebd066d3a2a5bfcee7f865
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-02-01 14:06:40 +0900

    Create VehicleController.cs

---
+++ Assets/OCSVehicle/Vehicle/Controller/VehicleController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    protected float[] _inputValues;
+    public float[] inputValues { get => this._inputValues; }
+}
