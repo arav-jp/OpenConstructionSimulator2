590cdf3e039d17e5e09526ef1b2586dd37b936e7
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-02-01 21:04:19 +0900

    Create Winker.cs

---
+++ Assets/OCSVehicle/Scripts/Equipment/Winker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winker : Equipment
+{
+    #region Objects
+    [SerializeField]
+    private Light[] _lights;
+    #endregion
+
+    #region Parameters
+    [SerializeField]
+    private float _frequency = 20.0f;
+    [SerializeField]
+    private float _maxIntensity = 1.0f;
+    #endregion
+
+    private bool _enabled;
+
+    public override void Update()
+    {
+        float intensity = _enabled ? 0.5f * (Mathf.Sin(Time.time * _frequency) + 1.0f) * _maxIntensity : 0.0f;
+        foreach (Light light in _lights)
+            light.intensity = intensity;
+
+
+        base.Update();
+    }
+
+    public override void UpdateInput(float[] inputValues)
+    {
+        if (inputValues.Length != 1)
+        {
+            return;
+        }
+        _enabled = inputValues[0]  > 0.5f ? true : false;
+    }
+}
