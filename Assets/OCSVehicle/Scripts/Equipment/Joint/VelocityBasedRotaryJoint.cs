37d616350e99f1e4d1cfa1884af204cdaf6250ac
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-02-03 18:52:51 +0900

    Update VelocityBasedRotaryJoint.cs

--- Assets/OCSVehicle/Scripts/Equipment/Joint/VelocityBasedRotaryJoint.cs
+++ Assets/OCSVehicle/Scripts/Equipment/Joint/VelocityBasedRotaryJoint.cs
@@ -10,9 +10,18 @@ public class VelocityBasedRotaryJoint : RotaryJoint
     [SerializeField]
     private float _T = 1.0f;
     [SerializeField]
-    private float _angleVelocity_min = 0.0f;
+    private float _angle_init = 0.0f;
     [SerializeField]
-    private float _angleVelocity_max = 180.0f;
+    private float _angle_min = 0.0f;
+    [SerializeField]
+    private float _angle_max = 180.0f;
+    [SerializeField]
+    private float _angleVelocity_min = -1.0f;
+    [SerializeField]
+    private float _angleVelocity_max = 1.0f;
+
+    [SerializeField]
+    private float _angle;
 
     public override void Start()
     {
@@ -28,11 +37,26 @@ public class VelocityBasedRotaryJoint : RotaryJoint
             _angleVelocity_min = _lagSystem.value_min;
             _angleVelocity_max = _lagSystem.value_max;
         }
+        _angle = _angle_init;
         base.Start();
     }
     public override void Update()
     {
-        _controlTarget.rotation *= Quaternion.AngleAxis(_lagSystem.value*Time.deltaTime, _axis);
+        float d_angle = _lagSystem.value*Time.deltaTime;
+        _controlTarget.rotation *= Quaternion.AngleAxis(d_angle, _axis);
+        _angle += d_angle;
+
+        if (_angle > _angle_max)
+        {
+            _controlTarget.rotation *= Quaternion.AngleAxis(- _angle + _angle_max, _axis);
+            _angle = _angle_max;
+        }
+        else if (_angle < _angle_min) 
+        {
+            _controlTarget.rotation *= Quaternion.AngleAxis(- _angle + _angle_min, _axis);
+            _angle = _angle_min;
+        }
+
         base.Update();
     }
 }
