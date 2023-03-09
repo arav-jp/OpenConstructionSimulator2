07e535591263e43dc0feb1f2a3402c588ab2bf82
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-03-09 13:59:33 +0900

    Create VehicleModule

---
+++ Assets/OCSVehicle/Scripts/VehicleModule.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class VehicleModule : MonoBehaviour
+{
+    [SerializeField] private InputInterfaceManager _input;
+    [SerializeField] private ProcessorLayerManager _transferFunction; 
+    [SerializeField] private OutputInterfaceManager _output;
+
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = this.transform;
+    }
+
+    private void Start()
+    {
+        if (!Application.isPlaying) return;
+
+        _input.transform = _transform;
+        _output.transform = _transform;
+
+        _input.Start();
+        _transferFunction.Start();
+        _output.Start();
+    }
+
+    private void Update()
+    {
+        _input.Update();
+
+        _transferFunction.input = _input.input;
+        _transferFunction.Update();
+        _output.output = _transferFunction.output;
+
+        _output.Update();
+        
+        if(!Application.isPlaying) EditorUtility.SetDirty(this);
+    }
+}
