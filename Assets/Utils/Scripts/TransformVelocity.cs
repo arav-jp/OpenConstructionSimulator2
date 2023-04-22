3907c22d3bf21b1cbb71f98b16b0c9a45ae1890b
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-04-22 12:28:29 +0900

    Create TransformVelocity.cs and visualizer

---
+++ Assets/Utils/Scripts/TransformVelocity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformVelocity : MonoBehaviour
+{
+    private Transform _transform;
+    private Vector3 _position_old;
+
+    private Vector3 _position;
+    [SerializeField, ReadOnly]
+    private Vector3 _velocity;
+
+    public Vector3 position { get => _position; }
+    public Vector3 velocity { get => _velocity; }
+
+    private void Awake()
+    {
+        _transform = this.transform;
+    }
+
+    private void OnEnable()
+    {
+        _position_old = _transform.position;
+    }
+
+    private void Update()
+    {
+        _position = _transform.position;
+        _velocity = (_position - _position_old) / Time.deltaTime;
+        _position_old = _position;
+    }
+}
