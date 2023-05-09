6473ada27cfd87e52b4b1f1c9efb1f6c8aece5f8
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-09 16:53:04 +0900

    Update SoilManager.cs

--- Assets/OCSTerrain/Scripts/Soil/SoilManager.cs
+++ Assets/OCSTerrain/Scripts/Soil/SoilManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SoilManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Inspector
+    [SerializeField]
+    private GameObject _soilObj;
+    [SerializeField]
+    private int _soilNum;
+    [SerializeField]
+    private float _soilSize_min;
+    #endregion
+
+    #region Parameters
+    private Soil[] _soils;
+    private Transform _transform;
+    #endregion
+
+    #region Properties
+    public float soilSize_min { get => _soilSize_min; }
+    #endregion
+
+    private void Awake()
     {
-        
+        _transform = transform;
+        _soils = new Soil[_soilNum];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for(int i = 0; i < _soilNum; i++)
+        {
+            var soil_obj = Instantiate(_soilObj);
+            soil_obj.transform.parent = _transform;
+            _soils[i] = soil_obj.GetComponent<Soil>();
+            _soils[i].Init(this);
+            _soils[i].Inactivate();
+        }
+    }
+
+    public void Spawn(Vector3 position, float volume)
+    {
+        foreach(Soil soil in _soils)
+        {
+            if (soil.IsActivated()) continue;
+            soil.Activate(position, volume);
+            break;
+        }
     }
 }
