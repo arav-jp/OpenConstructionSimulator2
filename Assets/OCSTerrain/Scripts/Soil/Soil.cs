9edf358bf55c81f9a85912fbc36c372b798efebd
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-09 16:52:58 +0900

    Update Soil.cs

--- Assets/OCSTerrain/Scripts/Soil/Soil.cs
+++ Assets/OCSTerrain/Scripts/Soil/Soil.cs
@@ -9,19 +9,21 @@ public class Soil : MonoBehaviour
     [SerializeField, ReadOnly]
     private float _diameter;
 
+    private SoilManager _manager;
     private Transform _transform;
+    private GameObject _gameObject;
 
     private const float _v2d_coef = 3.666873708f; // 12.0f / (5.0f * (3.0f + Mathf.Sqrt(5.0f))) * 2.0^3
 
-    public void Init(float volume)
+    public void Init(SoilManager manager)
     {
-        _volume = volume;
-        _diameter = Mathf.Pow(_volume * _v2d_coef, 1.0f / 3.0f);
+        _manager = manager;
     }
 
     private void Awake()
     {
         _transform = transform;
+        _gameObject = gameObject;
     }
 
     private void Start()
@@ -32,5 +34,23 @@ public class Soil : MonoBehaviour
     private void Update()
     {
         
+    }
+
+    public void Activate(Vector3 position, float volume)
+    {
+        _transform.position = position;
+        _diameter = Mathf.Pow(volume * _v2d_coef, 1.0f / 3.0f);
+        _transform.localScale = Vector3.one * _diameter;
+        _gameObject.SetActive(true);
+    }
+
+    public void Inactivate()
+    {
+        _gameObject.SetActive(false);
+    }
+
+    public bool IsActivated()
+    {
+        return _gameObject.activeSelf;
     }
 }
