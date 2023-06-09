a5509bf34a1e58efc4bbbf5933ed84b77adfe198
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-06-09 14:32:45 +0900

    Update WheelModule.cs

--- Assets/OCSVehicle/Runtime/Scripts/VehicleModules/WheelModule.cs
+++ Assets/OCSVehicle/Runtime/Scripts/VehicleModules/WheelModule.cs
@@ -9,10 +9,14 @@ namespace OCS.Vehicle
     {
         [SerializeField]
         private Wheel[] _wheels;
+
+        [SerializeField]
+        private float _magnification = 1.0f;
         public override Type moduleType { get => typeof(WheelModule); }
 
         public void Drive(float input)
         {
+            input *= _magnification;
             foreach(Wheel wheel in _wheels)
             {
                 wheel.Drive(input);
