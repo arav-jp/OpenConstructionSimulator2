05b9942bff11cefa6736c52003e79da25d1d822f
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-07-30 15:01:52 +0900

    Update BackhoeROSController.cs

--- Assets/OCSVehicleIO/Runtime/Scripts/Backhoe/BackhoeROSController.cs
+++ Assets/OCSVehicleIO/Runtime/Scripts/Backhoe/BackhoeROSController.cs
@@ -10,26 +10,49 @@ namespace OCS.VehicleIO
     public class BackhoeROSController : BackhoeController
     {
         [SerializeField]
-        private string _testTopicName = "joy/leftCrawler";
+        private string _leftCrawlerJoystick_topicName = "backhoe/leftCrawlerJoyStick";
+        [SerializeField]
+        private string _rightCrawlerJoystick_topicName = "backhoe/rightCrawlerJoyStick";
+        [SerializeField]
+        private string _jointJoystick_left_topicName = "backhoe/jointJoyStick_left";
+        [SerializeField]
+        private string _jointJoystick_right_topicName = "backhoe/jointJoystick_right";
 
         private ROSConnection _ros;
 
         private void Awake()
         {
-            // ROSオブジェクトの生成・取得
             _ros = ROSConnection.GetOrCreateInstance();
         }
 
         private void Start()
         {
-            // Subscriber設定
-            // Subscribe<メッセージ型>(トピック名, コールバック関数)で指定
-            _ros.Subscribe<JoyMsg>(_testTopicName, JoyCallback);
+            _ros.Subscribe<JoyMsg>(_leftCrawlerJoystick_topicName, LeftCrawlerJoyCallback);
+            _ros.Subscribe<JoyMsg>(_leftCrawlerJoystick_topicName, RightCrawlerJoyCallback);
+            _ros.Subscribe<JoyMsg>(_leftCrawlerJoystick_topicName, JointJoyLeftCallback);
+            _ros.Subscribe<JoyMsg>(_leftCrawlerJoystick_topicName, JointJoyRightCallback);
         }
 
-        private void JoyCallback(JoyMsg msg)
+        private void LeftCrawlerJoyCallback(JoyMsg msg)
         {
             _leftCrawlerJoystick.y = msg.axes[0] * 0.01f;
+        }
+
+        private void RightCrawlerJoyCallback(JoyMsg msg)
+        {
+            _rightCrawlerJoystick.y = msg.axes[0] * 0.01f;
+        }
+
+        private void JointJoyLeftCallback(JoyMsg msg)
+        {
+            _jointJoystick_left.x = msg.axes[0] * 0.01f;
+            _jointJoystick_left.y = msg.axes[1] * 0.01f;
+        }
+
+        private void JointJoyRightCallback(JoyMsg msg)
+        {
+            _jointJoystick_right.x = msg.axes[0] * 0.01f;
+            _jointJoystick_right.y = msg.axes[1] * 0.01f;
         }
     }
 }
