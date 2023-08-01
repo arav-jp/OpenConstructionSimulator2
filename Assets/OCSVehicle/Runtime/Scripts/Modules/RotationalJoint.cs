adc4e61cf4b554a58e3f30a56ba55251c95bf8fb
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-08-01 19:35:40 +0900

    Add param

--- Assets/OCSVehicle/Runtime/Scripts/Modules/RotationalJoint.cs
+++ Assets/OCSVehicle/Runtime/Scripts/Modules/RotationalJoint.cs
@@ -8,6 +8,8 @@ namespace OCS.Vehicle
     [RequireComponent(typeof(ArticulationBody))]
     public class RotationalJoint : MonoBehaviour
     {
+        [SerializeField]
+        private bool _isLimited;
         [SerializeField, ReadOnly]
         private float _speed;
 
@@ -27,7 +29,8 @@ namespace OCS.Vehicle
 
         private void Update()
         {
-            if (_speed != 0) _target_position = Mathf.Clamp(_joint.xDrive.target + _speed * Time.deltaTime, _lowerLimit, _upperLimit);
+            if (_speed != 0) _target_position = _joint.xDrive.target + _speed * Time.deltaTime;
+            if (_isLimited) _target_position = Mathf.Clamp(_target_position, _lowerLimit, _upperLimit);
             var xDrive = _joint.xDrive;
             xDrive.target = _target_position;
             xDrive.targetVelocity = _speed;
